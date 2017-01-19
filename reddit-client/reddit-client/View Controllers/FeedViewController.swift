bbfd703559a39a8be945d3f63a8260e0a44afe58
Pablo Romero <romero7116@gmail.com>
2017-01-18 21:27:54 -0300

    Created empty base project.

---
+++ reddit-client/reddit-client/View Controllers/FeedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FeedViewController.swift
+//  reddit-client
+//
+//  Created by Pablo Romero on 1/18/17.
+//  Copyright © 2017 Pablo Romero. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
