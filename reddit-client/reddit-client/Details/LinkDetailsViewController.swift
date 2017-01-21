c0517c3b6ca69d38d40b226bc228714e337b042c
Pablo Romero <romero7116@gmail.com>
2017-01-21 15:49:14 -0300

    LinkDetailsVC: added a spinner when the web content is being loaded.

--- reddit-client/reddit-client/Details/LinkDetailsViewController.swift
+++ reddit-client/reddit-client/Details/LinkDetailsViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 import WebKit
 
-class LinkDetailsViewController: DetailsViewController {
+class LinkDetailsViewController: DetailsViewController, WKNavigationDelegate {
 
     var webView: WKWebView!
+    var spinner: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         self.setupWebView()
@@ -19,12 +20,20 @@ class LinkDetailsViewController: DetailsViewController {
     }
 
     func setupWebView() {
+       
         let webConfiguration = WKWebViewConfiguration()
         self.webView = WKWebView(frame: self.view.bounds, configuration: webConfiguration)
         self.view.addSubview(self.webView)
+        
+        self.webView.autoresizingMask = [.flexibleWidth,
+                                         .flexibleHeight]
+        
+        self.webView.navigationDelegate = self
     }
     
     override func setup(withUrl url: URL) {
+       
+        self.presentLoadingMode()
         self.title = url.host
         let request = URLRequest(url: url)
         self.webView.load(request)
@@ -34,4 +43,37 @@ class LinkDetailsViewController: DetailsViewController {
     override func contentForActivityViewController() -> [Any] {
         return [NSURL(string: (self.currentURL?.absoluteString)!)!]
     }
+    
+    func webViewWebContentProcessDidTerminate(_ webView: WKWebView) {
+        self.removeLoadingMode()
+    }
+    
+    private func presentLoadingMode() {
+      
+        if self.spinner == nil
+        {
+            let spinner = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+            self.view.addSubview(spinner)
+            
+            spinner.startAnimating()
+            spinner.center = CGPoint(x: self.view.bounds.size.width / 2.0,
+                                     y: self.view.bounds.size.height / 2.0)
+            
+            spinner.autoresizingMask = [.flexibleBottomMargin,
+                                        .flexibleLeftMargin,
+                                        .flexibleRightMargin,
+                                        .flexibleTopMargin]
+            
+            self.spinner = spinner
+        }
+    }
+    
+    private func removeLoadingMode() {
+        
+        if let spinner = self.spinner {
+            spinner.removeFromSuperview()
+        }
+        self.spinner = nil
+    }
+    
 }
